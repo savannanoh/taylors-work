29fd910adaaf997a5b63742a90c4d6c0e21fd629
savannanoh <savannay@uw.edu>
2022-11-21 16:02:18 -0800

    changed to two columns

--- todo/src/App.js
+++ todo/src/App.js
@@ -10,49 +10,124 @@ import Entry from './Entry';
 
 function App() {
 
-  const [ todos, setTodos ] = useState(data);
+  const [todos, setTodos] = useState(data);
+  const [dones, setDones] = useState([]);
 
-  const handleToggle = (id) => {
+  const handleToggleFromTodo = (id) => {
+    console.log(id);
+    let copy = [...dones];
+    let move = null;
+    let filtered = todos.filter(task => {
+      if (task.id === Number(id)) {
+        move = task;
+      }
+      return task.id !== Number(id);
+    });
+    console.log(move);
+    setTodos(filtered);
+    if (move !== null) {
+      move.complete = !move.complete;
+      copy = [move, ...copy];
+      setDones(copy);
+    }
+    /*
     let mapped = todos.map(task => {
       return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
     });
     setTodos(mapped);
+    */
+  }
+
+  const handleToggleFromDone = (id) => {
+    console.log(id);
+    let copy = [...todos];
+    let move = null;
+    let filtered = dones.filter(task => {
+      if (task.id === Number(id)) {
+        move = task;
+      }
+      return task.id !== Number(id);
+    });
+    console.log(move);
+    setDones(filtered);
+    if (move !== null) {
+      move.complete = !move.complete;
+      copy = [move, ...copy];
+      setTodos(copy);
+    }
+    /*
+    let mapped = todos.map(task => {
+      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+    });
+    setTodos(mapped);
+    */
   }
 
   const handleFilter = () => {
+    setDones([]);
+    /*
     let filtered = todos.filter(task => {
       return !task.complete;
     });
     setTodos(filtered);
+    */
   }
 
-  const handleRemove = (id) => {
+  const handleRemoveTodo = (id) => {
     let filtered = todos.filter(task => {
       return task.id !== Number(id);
     });
     setTodos(filtered);
   }
 
-  const addTask = (userInput ) => {
+  const handleRemoveDone = (id) => {
+    let filtered = dones.filter(task => {
+      return task.id !== Number(id);
+    });
+    setDones(filtered);
+  }
+
+  const addTask = (userInput) => {
     let copy = [...todos];
-    copy = [{ id: todos.length + 1, task: userInput, complete: false }, ...copy];
+    copy = [{ id: todos.length + dones.length + 1, task: userInput, complete: false }, ...copy];
     setTodos(copy);
   }
+
+  let empty = (
+    <div className='empty'>
+      Nothing here yet
+    </div>
+  )
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>To Do List</h1>
       </header>
-      <AddTodo addTask={addTask}/>
-      <div className="todo-list">
-          {todos.map(todo => {
+      <div className='lists'>
+        <div className='left-side'>
+        {todos.length !== 0 ?
+          <div className="todo-list">
+            {todos.map(todo => {
               return (
-                  <Entry todo={todo} handleToggle={handleToggle} handleFilter={handleFilter} handleRemove={handleRemove}/>
+                <Entry todo={todo} handleToggle={handleToggleFromTodo} handleFilter={handleFilter} handleRemove={handleRemoveTodo} />
               )
-          })}
+            })}
+          </div> : empty}
+          <AddTodo addTask={addTask} />
+        </div>
+        <div className='right-side'>
+        {dones.length !== 0 ?
+          <div className="done-list">
+            {dones.map(done => {
+              return (
+                <Entry todo={done} handleToggle={handleToggleFromDone} handleFilter={handleFilter} handleRemove={handleRemoveDone} />
+              )
+            })}
+          </div> : empty}
+          <button className='clear-button' onClick={handleFilter}>Clear Completed</button>
+        </div>
       </div>
-      <button className='clear-button' onClick={handleFilter}>Clear Completed</button>
       <p>
         Being useful is all I know
       </p>
